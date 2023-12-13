e5eb23481213452be8b8a64d43f616e093e2c824
Botond Osvath <osvathbotond25@gmail.com>
2023-12-14 00:06:48 +0100

    Added username and password for mqtt

--- src/senders/mqtt.py
+++ src/senders/mqtt.py
@@ -10,12 +10,16 @@ class MqttSender(Sender):
     def setup(self, config: dict) -> None:
         address = config["address"]
         port = config["port"]
+        username = config.get("username", None)
+        password = config.get("password", None)
 
         self.client = mqtt.Client()
+        if username is not None and password is not None:
+            self.client.username_pw_set(username, password)
         self.client.connect(address, port)
 
     def teardown(self) -> None:
         self.client.disconnect()
-    
+
     def send_message(self, name: str, settings: dict, receiver: Receiver) -> None:
         self.client.publish(settings["topic"], json.dumps(receiver.to_json(name)))
